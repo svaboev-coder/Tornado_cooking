5256782ab5dfea89246d4ea692928efbd56b4988
Your Name <your.email@example.com>
2025-08-26 09:54:51 +0300

    чистка проекта: удалены ненужные файлы и дубликаты

--- start_client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Запуск клиентского приложения...
-start /B python client_app.py
-echo Приложение запущено в фоновом режиме
-echo Откройте браузер и перейдите на http://localhost:8080
-pause
-
-
-
--- stop_client.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Остановка клиентского приложения...
-taskkill /f /im python.exe
-echo Приложение остановлено
-pause
-
-
-
